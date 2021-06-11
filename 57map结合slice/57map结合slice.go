f6da9188993109bb1f67876bbb0157ea15f71d7c
Ang <623045191@qq.com>
2021-06-11 15:44:23 +0800

    6.11补充

--- 57map结合slice/57map结合slice.go
+++ 57map结合slice/57map结合slice.go
@@ -1,1 +1,16 @@
-package _7map结合slice
+package main
+
+func main() {
+	/*
+		map 和 slice的结合使用：
+			1. 创建map用于存储人的信息
+				name, age, sex, address
+
+			2. 每个map存储一个人的信息
+
+			3. 将这些map存入到slice中
+
+			4. 打印遍历输出
+
+	*/
+}
